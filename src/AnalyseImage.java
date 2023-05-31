dc51bbecb37d249d784e1a812055238e8764c577
Siiddy <113548091+Siiddy@users.noreply.github.com>
2023-05-31 16:57:31 +0200

    FiltreMedianColor

--- src/AnalyseImage.java
+++ src/AnalyseImage.java
@@ -9,40 +9,32 @@ public class AnalyseImage {
 	
 	public static Image FiltreMedian(Image input) {
 		
-	    int width = input.getXDim();
+		int width = input.getXDim();
 	    int height = input.getYDim();
 
-	    ByteImage newImgMedian=new ByteImage(width,height,1,1,1);
-
+	    ByteImage newImgMedian = new ByteImage(width, height, 1, 1, 3); 
 
 	    for (int y = 0; y < height; y++) {
 	        for (int x = 0; x < width; x++) {
-	            
-	            int tmp=0;
-	            List<Integer> values = new ArrayList<>();
-	           
+	            for (int b = 0; b < 3; b++) { 
+	                List<Integer> values = new ArrayList<>();
 
-	            for (int dy = -1; dy <= 1; dy++) {
-	                for (int dx = -1; dx <= 1; dx++) {
-	                    if (x + dx >= 0 && x + dx < width && y + dy >= 0 && y + dy < height) {
-	                        int valeurVoisin = input.getPixelXYBByte(x+dx, y+dy,0);
-	                     
-	                        values.add(valeurVoisin);
+	                for (int dy = -1; dy <= 1; dy++) {
+	                    for (int dx = -1; dx <= 1; dx++) {
+	                        if (x + dx >= 0 && x + dx < width && y + dy >= 0 && y + dy < height) {
+	                            int valeurVoisin = input.getPixelXYBByte(x + dx, y + dy, b);
+	                            values.add(valeurVoisin);
+	                        }
 	                    }
 	                }
-	            }
-	            
-	            Collections.sort(values);
 
-	            int valeurMedian = values.get((values.size() + 1) / 2 - 1);
-	            
-	            
-	            System.out.println("Valeur avant : "+tmp+" NOuvelle  Valeur : "+valeurMedian);
-	           
-	            newImgMedian.setPixelXYBByte(x, y,0,valeurMedian);
-	          
+	                Collections.sort(values);
+	                int valeurMedian = values.get((values.size() + 1) / 2 - 1);
+	                newImgMedian.setPixelXYBByte(x, y, b, valeurMedian);
+	            }
 	        }
 	    }
+
 	    return newImgMedian;
 	    //Viewer2D.exec(image);   
 	}
